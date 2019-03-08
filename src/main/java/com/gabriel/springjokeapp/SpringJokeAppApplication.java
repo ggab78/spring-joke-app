1da0e3cf3f927a09f74593af368665b92c630b9f
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2019-03-08 21:48:18 +0100

    implement java based configuration

--- src/main/java/com/gabriel/springjokeapp/SpringJokeAppApplication.java
+++ src/main/java/com/gabriel/springjokeapp/SpringJokeAppApplication.java
@@ -1,9 +1,13 @@
 package com.gabriel.springjokeapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringJokeAppApplication {
 
     public static void main(String[] args) {
